c6a3e6540c01b6dcf167201981ce0469490dd9b9
Samuel Brucksch <samuel@iternio.com>
2025-10-22 14:30:10 +0200

    add doc

--- packages/react-native-autoplay/src/types/Image.ts
+++ packages/react-native-autoplay/src/types/Image.ts
@@ -4,19 +4,21 @@ export type AutoImage = {
   name: GlyphName;
 
   /**
-   * sets the icon color used in light mode, on Android it is not always applied, depending on where the icon is used
+   * Sets the icon color used in light mode, on Android it is not always applied, depending on where the icon is used.
+   * For example in a GridTemplate or in a ListTemplate a colored icon works, where on a MapTemplate it does not.
    * @summary defaults to white if not specified
    */
   lightColor?: string;
 
   /**
-   * sets the icon color used in dark mode, on Android it is not always applied, depending on where the icon is used
+   * Sets the icon color used in dark mode, on Android it is not always applied, depending on where the icon is used
+   * For example in a GridTemplate or in a ListTemplate a colored icon works, where on a MapTemplate it does not.
    * @summary defaults to black if not specified
    */
   darkColor?: string;
 
   /**
-   * sets the background color, currently Android does not allow colors and converts everything to grayscale so we stick to transparent for this
+   * Sets the background color, currently Android does not allow colors and converts everything to grayscale so we stick to transparent for this
    * @summary defaults to transparent if not specified
    * @namespace iOS
    */
